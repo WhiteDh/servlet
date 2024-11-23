5be3053dd13e7d86e4fab9abe5bb43ca7dc79133
WHITE <dima.itsko.ua@gmail.com>
2024-11-23 23:23:09 +0200

    Added Thymeleaf and timezone cookie support

--- src/main/java/goit/servlet/TimeServlet.java
+++ src/main/java/goit/servlet/TimeServlet.java
@@ -1,41 +1,115 @@
-package com.example.time;
+package goit.servlet;
 
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.*;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
+
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        templateEngine = new TemplateEngine();
+
+
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix(getServletContext().getRealPath("/WEB-INF/templates/"));
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(templateEngine.getTemplateResolvers().size());
+
+        templateEngine.addTemplateResolver(resolver);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
 
+        // get arg for timezone
         String timezone = request.getParameter("timezone");
+
+        if (timezone == null) {
+            // get from cookies
+            String timezoneFromCookie = getTimezoneFromCookie(request);
+
+            if (timezoneFromCookie != null) {
+                timezone = timezoneFromCookie;
+            }
+            else {
+                timezone = "UTC";  // if not exist
+            }
+        }
 
         //чомусь знак "+" міняється на пробіл
         //зі знаком "-" такої проблеми немає
         if (timezone != null) {
             timezone = timezone.replace(" ", "+");
         }
-        System.out.println("time");
-        ZoneId zoneId = timezone != null ? ZoneId.of(timezone) : ZoneId.of("UTC");
+        // save to cookies if is valid
+        if (isValidTimezone(timezone)) {
+            setTimezoneInCookie(response, timezone);
+        }
 
-        String currentTime = DateTimeFormatter.ofPattern("yyyy-mm-dd hh:mm:ss z")
-                .withZone(zoneId)
-                .format(Instant.now());
+        // get the current time for a given time zone
+        ZoneId zoneId = ZoneId.of(timezone);
+        ZonedDateTime currentTime = ZonedDateTime.now(zoneId);
 
-        response.getWriter().write("<html><body>");
-        response.getWriter().write("<h1>Current Time:</h1>");
-        response.getWriter().write("<p>" + currentTime + "</p>");
-        response.getWriter().write("</body></html>");
+        // formating time
+        String formattedTime = currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
+
+        // template options
+        Map<String, Object> params = Map.of(
+                "time", formattedTime,
+                "timezone", timezone
+        );
+
+        // use thymeleaf
+        Context context = new Context(request.getLocale(), params);
+        templateEngine.process("time", context, response.getWriter());
+        response.getWriter().close();
+    }
+
+
+    //Checking the correct time zone
+    private boolean isValidTimezone(String timezone) {
+        try {
+            ZoneId.of(timezone);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // get timezote from  cookies
+    private String getTimezoneFromCookie(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("lastTimezone".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;  // if cookies do not exist
+    }
+
+    // save cookie
+    private void setTimezoneInCookie(HttpServletResponse response, String timezone) {
+        Cookie cookie = new Cookie("lastTimezone", timezone);
+        cookie.setMaxAge(60 * 60 * 5);  // period 5 hours
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
 }
--- src/main/java/goit/servlet/TimezoneValidateFilter.java
+++ src/main/java/goit/servlet/TimezoneValidateFilter.java
@@ -1,21 +1,15 @@
 package goit.servlet;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.time.ZoneId;
-import java.time.zone.ZoneRulesException;
-import java.util.TimeZone;
 
 @WebFilter("/time")
 public class TimezoneValidateFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String timezone = request.getParameter("timezone");
@@ -24,30 +18,30 @@ public class TimezoneValidateFilter implements Filter {
             timezone = timezone.replace(" ", "+");
 
             try {
-
-                //перетворюрюємо часовий пояс на ZoneId
-                System.out.println("try");
-                System.out.println(timezone);
+                // Перевірка, чи є валідний часовий пояс
                 ZoneId zone = ZoneId.of(timezone);
-                System.out.println(zone.toString());
             } catch (Exception e) {
-                // якщо виникає помилка, то повертаємо помилку 400 і відповідь
-                ((jakarta.servlet.http.HttpServletResponse) response).setStatus(400);
+                // Якщо помилка, відправляємо помилку 400 і відповідь
+                ((HttpServletResponse) response).setStatus(400);
                 response.setContentType("text/html");
                 response.getWriter().write("<html><body>");
-                response.getWriter().write("<h1>invalid timezone</h1>");
-                response.getWriter().write("<p> status is" + ((HttpServletResponse) response).getStatus() +"</p>");
+                response.getWriter().write("<h1>Invalid Timezone</h1>");
+                response.getWriter().write("<p>Status code: " + ((HttpServletResponse) response).getStatus() + "</p>");
                 response.getWriter().write("</body></html>");
 
                 return;
             }
         }
 
-        // якщо часовий пяос коректний,то продовжуємо обробку запиту
+        // Якщо часовий пояс правильний, продовжуємо обробку запиту
         chain.doFilter(request, response);
     }
 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
-
-
+    @Override
+    public void destroy() {
+    }
 }
